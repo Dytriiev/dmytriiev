fba6e68fad1eb72e21cc0ba327e267b815c37b65
DenDmik <dmikdenis@gmail.com>
2025-05-19 13:31:58 +0000

    дотатковый спосіб , щоб зовсім без циклів

--- MainBlock/task-1/index.js
+++ MainBlock/task-1/index.js
@@ -1,7 +1,10 @@
 const strArray = "[1, 2, [3, 4, [5]], 6]"
 console.log(JSON.parse(strArray))
 
+const arrayStr_2 = '[]'
+
 function summArrayStr(strArray){
+    
  const array = JSON.parse(strArray)
  function summArray(array){
   return  array.reduce((acc,item)=>{
@@ -13,5 +16,44 @@ function summArrayStr(strArray){
     },0)
 } return summArray(array)
 }
- let result1 = summArrayStr(strArray)
+ let result1 = summArrayStr( arrayStr_2)
  console.log(result1)
+ ////////////////////////////////////////////////////////
+
+ /// Або так , щоб зовсім без циклив.  ///////////////////////////////
+
+ function summArrayStr_2(strArray){
+    
+    const array = JSON.parse(strArray)
+    if(array.length === 0) return 0
+    function summArray_2(array){
+        let length = array.length
+     if(length === 1){
+     let r = array[array.length-1]
+     console.log(`r1:${r}`)
+       if(!Array.isArray(r)){
+    return r 
+   } else {
+    return summArray_2(r)
+   }
+     } else { 
+     let r = array[array.length-1]
+     array.pop()
+
+      if(!Array.isArray(r)){
+    return r + summArray_2(array)
+   } else {
+    return summArray_2(r) + summArray_2(array)
+   }
+     }
+    }
+    return summArray_2(array)
+ }
+
+
+ const res_2 = summArrayStr_2(arrayStr_2)
+ console.log(res_2)
+
+ 
+  
+ 
