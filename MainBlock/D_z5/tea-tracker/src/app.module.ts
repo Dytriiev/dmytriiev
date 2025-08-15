dd1478df80918ad82ba19f1b77e5681f2d997935
DenDmik <dmikdenis@gmail.com>
2025-08-15 12:14:14 +0300

    Добавил Throttle

--- MainBlock/D_z5/tea-tracker/src/app.module.ts
+++ MainBlock/D_z5/tea-tracker/src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { BrewModel } from './app.service';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [
-    ThrottlerModule.forRoot({
-      throttlers: [ ]
-    })
+imports: [
+    ThrottlerModule.forRootAsync({
+      useFactory: async () => [
+        {
+          // ttl: 60000,
+          // limit: 2
+           ttl: 1000, // 1 секунда (минимальное значение)
+          limit: Number.MAX_SAFE_INTEGER, // Очень большое число (~9 квадриллионов)
+        },
+      ],
+    }),
   ],
-
   controllers: [AppController],
-  providers: [BrewModel],
+  providers: [BrewModel,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
